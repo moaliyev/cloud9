a129e0835532196dedecb30d66619dd6ab211d77
sma12t <mehemmed05.aliyev@gmail.com>
2023-11-17 20:35:28 +0400

    mobile navbar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ import {
   FaX,
   FaAngleRight,
   FaPlus,
+  FaMinus,
 } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 
@@ -21,9 +22,13 @@ import { changeIsNavActive } from "../redux/slices/mobileSlice";
 
 // TRANSLATION
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 const Header = () => {
   const { t, i18n } = useTranslation();
+  const [isDropDownActiveApparel, setIsDropDownActiveApparel] = useState(false);
+  const [isDropDownActiveTeamWear, setIsDropDownActiveTeamWear] =
+    useState(false);
 
   const { isNavActive } = useSelector(state => state.mobile);
 
@@ -48,7 +53,6 @@ const Header = () => {
                 id="lang"
                 value={i18n.language}
                 onChange={e => i18n.changeLanguage(e.target.value)}>
-                <option value="">{t("header.language")}</option>
                 <option value="en">EN</option>
                 <option value="az">AZ</option>
                 <option value="tr">TR</option>
@@ -72,19 +76,47 @@ const Header = () => {
           <nav className={isNavActive ? "navBar active" : "navBar"}>
             <FaX onClick={() => dispatch(changeIsNavActive())} />
             <ul className="navList">
-              <li className="navItem longer">
-                <Link to="/">
-                  {t("header.navbar.apparel")} <FaPlus />
-                </Link>
+              <li
+                className={`navItem longer${
+                  isDropDownActiveApparel
+                    ? isDropDownActiveTeamWear
+                      ? " active expanded"
+                      : " active"
+                    : ""
+                }`}>
+                {isNavActive ? (
+                  <div
+                    onClick={() =>
+                      setIsDropDownActiveApparel(!isDropDownActiveApparel)
+                    }>
+                    {t("header.navbar.apparel")} <FaPlus /> <FaMinus />
+                  </div>
+                ) : (
+                  <Link>{t("header.navbar.apparel")}</Link>
+                )}
                 <ul className="dropDown">
                   <li className="dropDownItem">
                     <Link>New Arrivals</Link>
                   </li>
-                  <li className="dropDownItem">
-                    <Link>
-                      All Teamwear
-                      <FaAngleRight />
-                    </Link>
+                  <li
+                    className={`dropDownItem${
+                      isDropDownActiveTeamWear ? " active" : ""
+                    }`}>
+                    {isNavActive ? (
+                      <div
+                        onClick={() =>
+                          setIsDropDownActiveTeamWear(!isDropDownActiveTeamWear)
+                        }>
+                        All Teamwear
+                        <FaPlus />
+                        <FaMinus />
+                      </div>
+                    ) : (
+                      <Link>
+                        All Teamwear
+                        <FaAngleRight />
+                      </Link>
+                    )}
                     <ul className="insideDropDown">
                       <li className="insideDropDownItem">
                         <Link>2023 Pro Kit</Link>
@@ -131,11 +163,6 @@ const Header = () => {
               </li>
               <li className="navItem longer">
                 <Link to="/">{t("header.navbar.collaborations")}</Link>
-                <ul className="dropDown">
-                  <li className="dropDownItem">
-                    <Link>Cloud9 X PacSun</Link>
-                  </li>
-                </ul>
               </li>
               <li className="navItem">
                 <Link to="/">{t("header.navbar.collections")}</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,10 @@ const Home = () => {
   return (
     <section className="hero">
       <div className="container">
-        <div className="row">
-          
-        </div>
+        <div className="row"></div>
       </div>
     </section>
-  )
+  );
 };
 
 export default Home;
